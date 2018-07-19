37dfe56d6c9417245923ba4668771d5d06de0a9a
kalinichenko364 <kalinichenko364@gmail.com>
2018-07-19 16:42:52 +0300

    edit_1

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,60 @@
-import sayHello from './lib/sayHello.js';
+import 'slick-carousel';
 
-sayHello();
 
-  
+// burger
+
 $('.js-open-menu').on('click', function(e) {
   $('.navs').toggleClass('active');
   $(this).toggleClass('active');
 });
+
+
+// slick-carousel
+
+$('.cards__list').slick({
+  infinite: true,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 1000,
+  pauseOnHover: true,
+  responsive: [
+    {
+      breakpoint: 900,
+      settings: {
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 550,
+      settings: {
+        slidesToShow: 2
+      }
+    },
+    {
+      breakpoint: 380,
+      settings: {
+        slidesToShow: 1
+      }
+    }
+  ]
+  ,
+  prevArrow: '<button class="btn-slick btn-slick--prev" type="button"><img src="img/arrow-prev.svg"></button>'
+  ,
+  nextArrow: '<button class="btn-slick btn-slick--next" type="button"><img src="img/arrow-next.svg"></button>'
+});
+
+// let slickInit = '.slick-initialized';
+// function adaptiveSliders() {
+//   if($(document).width() <= 768) {
+//     $('.workers').not(slickInit).slick({
+//       slidesToShow: 1,
+//       prevArrow: '<button class="btn-slick" type="button"><img src="img/arrow-left.svg"></button>',
+//       nextArrow: '<button class="btn-slick" type="button"><img src="img/arrow-right.svg"></button>'
+//     });
+//   } else {
+//     if($('.workers' + slickInit).length >= 1) {
+//       $('.workers').slick('unslick');
+//     }
+//   }
+// }
